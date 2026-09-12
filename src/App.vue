f930a10b0635d4026b0ec91598d782d1e89bb0a7
ardss <92158419+ardss@users.noreply.github.com>
2026-09-12 08:30:22 +0800

    release: bump to v1.0.0 (first public release)

--- src/App.vue
+++ src/App.vue
@@ -419,7 +419,7 @@ onBeforeUnmount(() => {
             <kbd class="kbd">Alt + V</kbd>
           </div>
         </div>
-        <div class="about">{{ t('about') }} ClipVault v0.3.0 · MIT</div>
+        <div class="about">{{ t('about') }} ClipVault v1.0.0 · MIT</div>
       </div>
     </div>
 
